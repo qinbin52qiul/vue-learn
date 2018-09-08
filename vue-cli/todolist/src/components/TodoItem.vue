b51581e72334e85ae77dd42da8f012e430d2c613
qinbin52qiul <2538323140@qq.com>
2018-09-09 00:48:12 +0800

    vue-cli 实现简单的 todolist

---
+++ vue-cli/todolist/src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ["content", "index"],
+  methods: {
+    handleDelete() {
+      this.$emit("delete", this.index);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
