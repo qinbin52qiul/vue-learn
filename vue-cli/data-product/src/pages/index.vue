1d454bcfc3924b274ca334d7a056e227136c3bd5
qinbin52qiul <2538323140@qq.com>
2018-09-14 17:13:45 +0800

    首页最新消息和信息列表

--- vue-cli/data-product/src/pages/index.vue
+++ vue-cli/data-product/src/pages/index.vue
@@ -3,38 +3,100 @@
     <div class="index-left">
       <div class="index-left-block">
         <h2>全部产品</h2>
-        <template v-for="product in productList">
-          <h3>{{ product.title }}</h3>
-          <ul>
-            <li v-for="item in product.list">
+        <template v-for="(product, index) in productList">
+          <h3 :key="index">{{ product.title }}</h3>
+          <ul :key="index">
+            <li v-for="(item, index) in product.list" :key="index">
               <a :href="item.url">{{ item.name }}</a>
               <span v-if="item.hot" class="hot-tag">HOT</span>
             </li>
           </ul>
-          <div v-if="!product.last" class="hr"></div>
+          <div v-if="!product.last" class="hr" :key="index"></div>
         </template>
-
+      </div>
         <div class="index-left-block lastest-news">
           <h2>最新消息</h2>
           <ul>
-
+            <li v-for="(item, index) in newsList" :key="index">
+              <a :href="item.url">{{ item.title }}</a>
+            </li>
           </ul>
         </div>
       </div>
       <div class="index-right">
-        <slide-show :slides="slides"></slide-show>
         <div class="index-board-list">
-
+          <div 
+            class="index-board-item" 
+            v-for="(item, index) in boardList" 
+            :key="index"
+            :class="[{'line-last' : index % 2 !== 0}, 'index-board-' + item.id]"
+          >
+            <div class="index-board-item-inner">
+              <h2>{{ item.title }}</h2>
+              <p>{{ item.description}}</p>
+              <div class="index-board-button">
+                <a href="" class="button">立即购买</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      boardList: [
+        {
+          title: '开放产品',
+          description: '开放产品是一款开放产品',
+          id: 'car',
+          toKey: 'analysis',
+          saleout: false
+        },
+        {
+          title: '品牌营销',
+          description: '品牌营销帮助你的产品更好地找到定位',
+          id: 'earth',
+          toKey: 'count',
+          saleout: false
+        },
+        {
+          title: '使命必达',
+          description: '使命必达快速迭代永远保持最前端的速度',
+          id: 'loud',
+          toKey: 'forecast',
+          saleout: true
+        },
+        {
+          title: '勇攀高峰',
+          description: '帮你勇闯高峰，到达事业的顶峰',
+          id: 'hill',
+          toKey: 'publish',
+          saleout: false
+        }
+      ],
+      newsList: [
+        {
+          title: "数据统计",
+          url: "detail/count"
+        },
+        {
+          title: "数据预测",
+          url: "detail/forecast"
+        },
+        {
+          title: "流量分析",
+          url: "detail/analysis",
+          hot: true
+        },
+        {
+          title: "广告发布",
+          url: "detail/publish"
+        }
+      ],
       productList: {
         pc: {
           title: "PC产品",
