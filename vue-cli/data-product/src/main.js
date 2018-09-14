066f6c478beb36b8d3dd744d23cc9a9247d5cb34
qinbin52qiul <2538323140@qq.com>
2018-09-14 16:35:44 +0800

    初始化项目

---
+++ vue-cli/data-product/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Layout from './components/layout'
+import IndexPage from './pages/index'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
