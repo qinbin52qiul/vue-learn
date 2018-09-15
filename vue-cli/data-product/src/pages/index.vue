34306880fcad660d13858ae3a55eb0a0a293e28d
qinbin52qiul <2538323140@qq.com>
2018-09-15 11:23:26 +0800

    首页幻灯片展示完善

--- vue-cli/data-product/src/pages/index.vue
+++ vue-cli/data-product/src/pages/index.vue
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div class="index-right">
-      <slide-show :slides="slides"></slide-show>
+      <slide-show :slides="slides" :inv="slideSpeed" @onchange="doSomethingOnSlideChange"></slide-show>
       <div class="index-board-list">
         <div class="index-board-item" v-for="(item, index) in boardList" :key="index" :class="[{'line-last' : index % 2 !== 0}, 'index-board-' + item.id]">
           <div class="index-board-item-inner">
@@ -47,7 +47,7 @@ export default {
   components: {
     slideShow
   },
-  created: function() {
+  /*created: function() {
     this.$http.post("http://localhost:3000/getNewsList")
     .then(data => {
       console.log(data);
@@ -55,9 +55,15 @@ export default {
       err => {
         console.log(err);
       };
+  },*/
+  methods: {
+    doSomethingOnSlideChange(index) {
+      console.log("doSomethingOnSlideChange run!" + index)
+    }
   },
   data() {
     return {
+      slideSpeed: 2000,
       slides: [
         {
           src: require('../assets/slideShow/pic1.jpg'),
